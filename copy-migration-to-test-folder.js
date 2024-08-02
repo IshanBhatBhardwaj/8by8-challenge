eefe258751888b7755f3c382d9e499c117abef7c
IshanBhatBhardwaj <ishanbhatbhardwaj@icloud.com>
2024-08-02 13:27:43 -0400

    Created a script to copy the migration file into the test directory

---
+++ copy-migration-to-test-folder.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourcePath = path.join(
+  __dirname,
+  'supabase/migrations',
+  '20240711063356_initial_schema.sql',
+);
+const destPath = path.join(
+  __dirname,
+  'src/__tests__/supabase/migrations',
+  '20240711063356_initial_schema.sql',
+);
+
+fs.copyFile(sourcePath, destPath, err => {
+  if (err) {
+    console.error('Error copying the file:', err);
+  }
+});
